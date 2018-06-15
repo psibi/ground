5cf7163f8a60c4c62090418c43746d65ef950fa9
Sibi Prabakaran <sibi@psibi.in>
2018-06-16 03:24:43 +0530

    Add note

--- chapter4/code/square.s
+++ chapter4/code/square.s
@@ -24,6 +24,8 @@ square:
 
         imull %eax, %eax
 
+# This code demonstrates how after execution of square section the code automatically jumps to end_square. So note that the ordering is important.        
+
 end_square:
         movl %ebp, %esp
         popl %ebp
