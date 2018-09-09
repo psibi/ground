b2e2a6f95dc8b9cff53bd68aa15b0d2ae55d00ae
Sibi Prabakaran <sibi@psibi.in>
2018-09-10 01:13:40 +0530

    Add code

---
+++ chapter5/code/test3.s
@@ -0,0 +1,23 @@
+# $ as --gen-debug --gdwarf-2 --32 test3.s -o test3.o
+# $ ld -m elf_i386 test3.o -o test3
+# $ ./test3
+# $ echo $status
+# 8
+
+
+        .section .bss
+
+        .lcomm SAMPLE_BUFFER, 8
+
+        .section .text
+
+        .globl _start
+
+_start:
+
+        movl $8,SAMPLE_BUFFER
+        mov $0, %ecx
+        mov SAMPLE_BUFFER(,%ecx, 4), %ebx
+
+        movl $1, %eax
+        int $0x80
